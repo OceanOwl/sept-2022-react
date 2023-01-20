1c990dd301976f995450e2778186000aef72c4d4
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2023-01-20 16:50:23 +0100

    lesson2_hw_part2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import {useEffect, useState} from "react";
+import {axiosService} from "./components/api/axiosService";
+
 const App = () => {
+    const [year, setYear]=useState([])
+    useEffect(()=>{
+        axiosService.get('')
+            .then(value=>value.data)
+            .then(value=>setYear([...value]))
+
+
+
+
+    },[])
     return (
         <div>
+            {year.filter(item=>item.launch_year !=='2020').map(item=>{
+                return(
+                    <div key={item.flight_number}>
+                        {item.flight_number} {item.mission_name} {item.launch_year}
+                    </div>
+                )
 
+
+            })}
         </div>
     );
 };
