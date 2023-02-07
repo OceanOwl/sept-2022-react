97109dfd0cc1058c41f5699b6c37516c0df4a1be
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2023-02-07 15:45:28 +0100

    lesson5_hw

--- src/reducers/carReducer.js
+++ src/reducers/carReducer.js
@@ -2,12 +2,12 @@ const carReducerActionType = {
     ADD: 'ADD',
     DELETE_BY_ID: 'DELETE_BY_ID'
 }
-const initialArr = () => []
+const initialArr = () => [];
 
-const actions={
-    ADD:(value)=>({type:carReducerActionType.ADD,payload:value}),
-    DELETE_BY_ID:(id)=>({type:carReducerActionType.DELETE_BY_ID,payload:id})
-}
+const actions = {
+    ADD: (value) => ({type: carReducerActionType.ADD, payload: value}),
+    DELETE_BY_ID: (id) => ({type: carReducerActionType.DELETE_BY_ID, payload: id})
+};
 
 const reducerFunc = (state, action) => {
     switch (action.type) {
@@ -15,15 +15,16 @@ const reducerFunc = (state, action) => {
             const slice = state.slice(-1);
             const id = slice.length ? slice[0].id + 1 : 0
             return [state, {id, ...action.payload}]
+
         case carReducerActionType.DELETE_BY_ID:
             const findIndex = state.findIndex(data => data.id === action.payload);
             state.splice(findIndex, 1)
             return [...state]
     }
 
-}
+};
 export {
     reducerFunc,
     actions,
     initialArr
-}
+};
