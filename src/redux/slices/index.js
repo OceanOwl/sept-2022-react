3cf93a75a29170a9c12e75c898dfc1f2b3a12880
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2023-02-07 19:49:45 +0100

    lesson6_hw

--- src/redux/slices/index.js
+++ src/redux/slices/index.js
@@ -8,75 +8,62 @@ const initialState = {
     selectedPost: null
 };
 
-createAsyncThunk(
-    'postSlice/DELETE_BY_ID',
-    async ({id}, rejectWithValue)=>{
+        const getAll = createAsyncThunk(
+            'postSlice/getAll',
+            async (_, {rejectWithValue}) => {
+                try {
+                    const {data} = await postService.getAll();
+                    return data
+                } catch (e) {
+                    return rejectWithValue(e.response.data)
+                }
 
-        const {data} = await postService.getById(id);
-
-    }
-)
+            }
+        )
 
 
+        const postSlice = createSlice({
+            name: 'postSlice',
+            initialState,
+            reducers: {
+                // getAll: (state, action) => {
+                //     state.posts = action.payload
+                // },
+                setSelectedPost: (state, action) => {
+                    state.selectedPost = action.payload
+                }
+            },
+            extraReducers:
+            //     [getAll.fulfilled]:(state, action)=>{
+            //         state.posts = action.payload
+            //     }
 
-const getAll = createAsyncThunk(
-    'postSlice/getAll',
-    async (_, {rejectWithValue}) => {
-        try {
-            const {data} = await postService.getAll();
-            return data
-        } catch (e) {
-            return rejectWithValue(e.response.data)
+                builder =>
+                    builder
+                        .addCase(getAll.fulfilled, (state, action) => {
+                            state.loading = false
+                            state.posts = action.payload
+                        })
+                        .addCase(getAll.rejected, (state, action) => {
+                            state.loading = false
+                            state.errors = action.payload
+
+                        })
+                        .addCase(getAll.pending, (state) => {
+                            state.loading = true
+
+                        })
+
+        });
+
+        const {reducer: postReducer, actions: {setSelectedPost}} = postSlice;
+
+        const postActions = {
+            setSelectedPost,
+            getAll
+        };
+
+        export {
+            postReducer,
+            postActions
         }
-
-    }
-)
-
-
-const postSlice = createSlice({
-    name: 'postSlice',
-    initialState,
-    reducers: {
-        // getAll: (state, action) => {
-        //     state.posts = action.payload
-        // },
-        setSelectedPost: (state, action) => {
-            state.selectedPost = action.payload
-        }
-    },
-    extraReducers:
-    //     [getAll.fulfilled]:(state, action)=>{
-    //         state.posts = action.payload
-    //     }
-
-            builder =>
-        builder
-            .addCase(getAll.fulfilled, (state, action) => {
-                state.loading = false
-                state.posts = action.payload
-            })
-            .addCase(getAll.rejected, (state, action) => {
-                state.loading = false
-                state.errors = action.payload
-
-            })
-            .addCase(getAll.pending, (state, action) => {
-                state.loading = true
-
-            })
-            // .addCase(DELETE_BY_ID.fulfield)
-
-
-});
-
-const {reducer: postReducer, actions: {setSelectedPost}} = postSlice;
-
-const postActions = {
-    setSelectedPost,
-    getAll
-};
-
-export {
-    postReducer,
-    postActions
-}
